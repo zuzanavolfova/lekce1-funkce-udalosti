c845b5f7dea68595c2f92a300f29cf787afb6d9a
HanaPos <h.hedbavna@gmail.com>
2021-03-08 16:52:50 +0100

    [BUGFIX] typo correction

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // tady je místo pro náš program
 
-document.querySelector('#vysledek').innerHTML = secti(4, 5);
+document.querySelector("#vysledek").innerHTML = secti(4, 5);
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
- * 
- * @param {int} a 
- * @param {int} b 
+ *
+ * @param {int} a
+ * @param {int} b
  */
 function secti(a, b) {
-	let c = a + b;
-	return c;
+  let c = a + b;
+  return c;
 }
 
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
-	alert('Gratulace, právě si spustila tuto funkci!');
+  alert("Gratulace, právě jsi spustila tuto funkci!");
 }
